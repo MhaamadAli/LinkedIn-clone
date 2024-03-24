d86459451b28b742ef1767f36f751dca8fdfa690
MhaamadAli <m.alialaawar@gmail.com>
2024-03-24 13:56:39 +0200

    finished and tested the profile api

---
+++ server/profile.php
@@ -0,0 +1,26 @@
+<?php
+include './connection.php';
+
+$userID = $_POST['userID'];
+$bio = $_POST['bio'];
+$summary = $_POST['summary'];
+$profile_img = $_POST['profile_img'];
+
+$query = $mysqli -> prepare('INSERT INTO profiles(userID, bio, summary, profileImg ) VALUES (?,?,?,?)');
+$query -> bind_param('isss', $userID, $bio, $summary, $profile_img);
+if($query->execute()) {
+    $profile = new stdClass();
+    $profile->id = $userID;
+    $profile->bio = $bio;
+    $profile->summary = $summary;
+    $profile->profile_img = $profile_img;
+    $response['message'] = "profile updated successfully";
+    $response['profile'] = $profile;
+
+} else {
+    $response['message'] = "profile not updated";
+}
+
+echo json_encode($response);
+
+$mysqli ->close();
